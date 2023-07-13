be89296f59d0cb1d40d7c753fcaff15acf7df5ea
Pablo Vasquez <vascor.pablo@gmail.com>
2023-07-13 18:50:01 -0400

    inicicio del proyecto

---
+++ inventario.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def inventario():
+    input(""" SISTEMA DE GESTION DE INVENTARIO
+----------------------------------
+
+Funciones: 
+        
+1.- Ver inventario.
+2.-Ver producto.
+2.- Agregar producto.
+3.-Quitar producto.
+
+Ingresa la función que deseas realizar --> """)
+    inventario = {
+        
+    }
+    
+    def mostrar_inventario():
+        print(inventario)
+        
+        
+inventario()        
+        
+        
+        
