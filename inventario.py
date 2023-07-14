325505d0dcfd9c26a8d29f2397e8ee4cae87b83e
Pablo Vasquez <vascor.pablo@gmail.com>
2023-07-14 18:49:11 -0400

    CASI TERMINANDO TIPOGRAFIA

--- inventario.py
+++ inventario.py
@@ -4,25 +4,33 @@ import sys
 
 # FUNCIÓN PRINCIPAL QUE CONTIENE EL PROGRAMA DE GESTION DE INVENTARIO
 # def inventario():
-        
-print(""" ___________________________________________________________________________________________________
-| 
-|  ________    _    ________
-| |  ______|  |_|  |  ______|
-| | |______    _   | |______
-| |______  |  | |  |______  |
-|  ______| |  | |   ______| |
-| |________|  |_|  |________|
-|
--------------------------------------""")   
+print("""                   ________    _    ________    _________    ________    __    __    ________         _______     ________
+                  |  ______|  |_|  |  ______|  |___   ___|  |  ______|  |  \\__/  |  |  ____  |       |  ____ \\   | _______|
+                  | |______    _   | |______       | |      | |______   |        |  | |____| |       | |    \\ |  | |______
+                  |______  |  | |  |______  |      | |      |  ______|  | |\\__/| |  |  ____  |       | |    | |  |  ______|
+                   ______| |  | |   ______| |      | |      | |______   | |    | |  | |    | |       | |____/ |  | |______
+                  |________|  |_|  |________|      |_|      |________|  |_|    |_|  |_|    |_|       |_______/   |________|
+                     ________    ________    ________    _________    _    ________    __   __         _______     ________
+                    |  ______|  |  ______|  | _______|  |___   ___|  |_|  |  ____  |  |  \\ |  |       |  ____ \\   | _______|
+                    | |   ___   | |______   | |______       | |       _   | |    | |  |   \\|  |       | |    \\ |  | |______
+                    | |  |_  |  |  ______|  |______  |      | |      | |  | |    | |  |       |       | |    | |  |  ______|
+                    | |____| |  | |______    ______| |      | |      | |  | |____| |  |  |\\   |       | |____/ |  | |______
+                    |________|  |________|  |________|      |_|      |_|  |________|  |__| \\__|       |_______/   |________|
+                       _    __   __   __        __   ________    __
+                      |_|  |  \\ |  |  \\ \\      / /  |  ______|  |
+                       _   |   \\|  |   \\ \\    / /   | |______   |
+                      | |  |       |    \\ \\  / /    |  ______|  |
+                      | |  |  |\\   |     \\ \\/ /     | |______   |  |\\   |
+                      |_|  |__| \\__|      \\__/      |________|  |__| \\__|
+                      """)   
 # VALIDACIÓN USUARIOS
 
 
-print(f"""|      Ingrese su inacap mail      
-    {input("hola")}|""")
+# print(f"""|      Ingrese su inacap mail      
+#     {input("hola")}|""")
 
 
-contrasena = input("Ingrese la contraseña ")
+# contrasena = input("Ingrese la contraseña ")
 usuarios_validos = ("agustin.segovia02@inacapmail.cl","cristian.quintanilla08@inacapmail.cl","Miguel","nicolas.riquelme40@inacapmail.cl","pablo.vasquez41@inacapmail.cl") 
     
 
@@ -31,6 +39,9 @@ usuarios_validos = ("agustin.segovia02@inacapmail.cl","cristian.quintanilla08@in
     
 inventario = np.array([["PRODUCTO","CÓDIGO","TIPO","PRECIO"],["Martillo","115","Manual","5000"],["Serrucho","045","Manual","5500"],["desatornillador","023","Manual","3500"]])
     
+    
+def busqueda_binaria():
+        return 0
     # print(inventario)
     
 #     input(""" SISTEMA DE GESTION DE INVENTARIO
@@ -49,8 +60,8 @@ inventario = np.array([["PRODUCTO","CÓDIGO","TIPO","PRECIO"],["Martillo","115",
     # def mostrar_inventario():
     #     print(inventario)
         
-def mostrar_producto():
-        return 0    
+# def mostrar_producto():
+#         return 0    
     
         
         
